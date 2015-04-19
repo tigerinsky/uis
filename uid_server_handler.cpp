baf31351108845c944699d1b526fc49ebfc2576d
dingsnow <xjtudc@126.com>
2015-04-19 20:04:24 +0800

    change comake

--- uid_server_handler.cpp
+++ uid_server_handler.cpp
@@ -2,12 +2,12 @@
 
 namespace tis {
 
-    int64_t UidServerHandler::get_id(const std::string& topic) {
-        // Your implementation goes here
-        printf("get_id\n");
-        int64_t uid = 0L;
-        return uid;
-    }
+int64_t UidServerHandler::get_id(const std::string& topic) {
+    // Your implementation goes here
+    printf("get_id\n");
+    int64_t uid = 0L;
+    return uid;
+}
 
 }
 
