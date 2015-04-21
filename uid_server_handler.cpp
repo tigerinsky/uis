e7765fb4dc23bcf2f1a4cb19d2c22a78c281b16b
dingsnow <xjtudc@126.com>
2015-04-21 21:54:40 +0800

    add topic data

--- uid_server_handler.cpp
+++ uid_server_handler.cpp
@@ -9,6 +9,34 @@ using std::endl;
 
 namespace tis {
 
+//生成topic的数据
+int UidServerHandler::init(int topic_num,
+        std::string& topic_names) {
+    topic_data temp;
+    topic_datas_[default_topic_] = temp;
+
+    if (topic_names.size() <= 0) {
+        return 0;
+    }
+
+    string::size_type start = 0;
+    string::size_type position = topic_names.find_first_of(';');
+    while (topic_names.npos != position) {
+        std::string topic(topic_names.substr(start, position - start));
+        topic_data temp;
+        topic_datas_[topic] = temp;
+        start = position + 1;
+        position = topic_names.find_first_of(';', start);
+    }
+
+    if (start < topic_names.npos) {
+        std::string topic(topic_names.substr(start));
+        topic_data temp;
+        topic_datas_[topic] = temp;
+    }
+
+    return 0;
+}
 uint64_t UidServerHandler::get_time() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -22,14 +50,20 @@ int64_t UidServerHandler::get_id(const std::string& topic) {
     // Your implementation goes here
     LOG(INFO) << "get_id" << endl;
     int64_t uid = 0L;
-    
+    TOPIC_MAP::iterator iter = topic_datas_.find(topic);
+    if (topic_datas_.end() == iter) {
+        iter = topic_datas_.find(default_topic_);
+    }
+
+    pthread_mutex_lock(&((iter->second).mutex_));
     uint64_t timestamp = get_time();
-    if (last_timestamp_ == timestamp) {
+    if ((iter->second).last_timestamp_ == timestamp) {
 
     } else {
 
     }
 
+    pthread_mutex_unlock(&((iter->second).mutex_));
     return uid;
 }
 
--- uid_server_handler.h
+++ uid_server_handler.h
@@ -2,19 +2,55 @@
 #define __UID_SERVER_HANDLER_H_
 
 #include <string>
+#include <map>
+#include <pthread.h>
 #include "Thriftfiles/UidServer.h"
 
 using namespace ::apache::thrift;
 using std::string;
+using std::map;
 
 namespace tis {
 
+struct topic_data {
+    pthread_mutex_t mutex_;
+    int32_t sequence_;
+    uint64_t last_timestamp_;
+
+    topic_data() {
+        pthread_mutex_init(&mutex_, NULL);
+        sequence_ = 0;
+        last_timestamp_ = 0L;
+    }
+
+    ~topic_data() {
+        pthread_mutex_destroy(&mutex_);
+    }
+
+    topic_data(const topic_data& other) {
+        this->mutex_ = other.mutex_;
+        this->sequence_ = other.sequence_;
+        this->last_timestamp_ = other.last_timestamp_;
+    }
+    
+    topic_data& operator = (const topic_data& other) {
+        this->mutex_ = other.mutex_;
+        this->sequence_ = other.sequence_;
+        this->last_timestamp_ = other.last_timestamp_;
+    }
+};
+
+typedef std::map<std::string, topic_data> TOPIC_MAP;
+
 class UidServerHandler : virtual public UidServerIf {
 public:
-    UidServerHandler():sequence_(0L), last_timestamp_(0L) {
+    UidServerHandler() {
         // Your initialization goes here
         time_epoch_ = 1288834974657L;
+        default_topic_.assign("default");
     }
+
+    int init(int topic_num, std::string& topic_names);
 
     int64_t get_id(const std::string& topic);
 
@@ -22,8 +58,8 @@ private:
     uint64_t get_time();
 
 private:
-    int32_t sequence_;
-    uint64_t last_timestamp_;
+    TOPIC_MAP topic_datas_;
+    std::string default_topic_;
     uint64_t time_epoch_; 
 };
 
