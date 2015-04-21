92f3f98aa361e93e220b12921b98841e211a5c4c
dingsnow <xjtudc@126.com>
2015-04-21 20:59:18 +0800

    add a little

--- uid_server_handler.cpp
+++ uid_server_handler.cpp
@@ -1,11 +1,35 @@
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "glog/logging.h"
+
 #include "uid_server_handler.h"
+
+using std::endl;
 
 namespace tis {
 
+uint64_t UidServerHandler::get_time() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    uint64_t time = tv.tv_usec;
+    time /= 1000;
+    time += (tv.tv_sec * 1000);
+    return time;
+}
+
 int64_t UidServerHandler::get_id(const std::string& topic) {
     // Your implementation goes here
-    printf("get_id\n");
+    LOG(INFO) << "get_id" << endl;
     int64_t uid = 0L;
+    
+    uint64_t timestamp = get_time();
+    if (last_timestamp_ == timestamp) {
+
+    } else {
+
+    }
+
     return uid;
 }
 
--- uid_server_handler.h
+++ uid_server_handler.h
@@ -1,19 +1,30 @@
 #ifndef __UID_SERVER_HANDLER_H_
 #define __UID_SERVER_HANDLER_H_
 
+#include <string>
 #include "Thriftfiles/UidServer.h"
 
 using namespace ::apache::thrift;
+using std::string;
 
 namespace tis {
 
 class UidServerHandler : virtual public UidServerIf {
 public:
-    UidServerHandler() {
-    // Your initialization goes here
+    UidServerHandler():sequence_(0L), last_timestamp_(0L) {
+        // Your initialization goes here
+        time_epoch_ = 1288834974657L;
     }
 
     int64_t get_id(const std::string& topic);
+
+private:
+    uint64_t get_time();
+
+private:
+    int32_t sequence_;
+    uint64_t last_timestamp_;
+    uint64_t time_epoch_; 
 };
 
 }
